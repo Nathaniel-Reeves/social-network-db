beafc588c0e6eafcde342784b6a48fc98f0f0257
Marie Sewell <mariesewell44@gmail.com>
2023-02-27 23:21:39 -0700

    most active user done

--- models.py
+++ models.py
@@ -301,3 +301,38 @@ def fetch_post_feed():
     conn.close()
     return posts
 
+def get_most_active_user():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    # Get all users
+    cursor.execute('SELECT _id, username FROM people')
+    users = cursor.fetchall()
+
+    # Initialize dictionary to keep track of activity counts
+    activity_counts = {}
+    for user in users:
+        activity_counts[user[0]] = 0
+
+    # Get counts of posts for each user
+    cursor.execute('SELECT author_id, COUNT(*) FROM posts GROUP BY author_id')
+    post_counts = cursor.fetchall()
+    for post_count in post_counts:
+        activity_counts[post_count[0]] += post_count[1]
+
+    # Get counts of comments for each user
+    cursor.execute('SELECT author_id, COUNT(*) FROM comments GROUP BY author_id')
+    comment_counts = cursor.fetchall()
+    for comment_count in comment_counts:
+        activity_counts[comment_count[0]] += comment_count[1]
+
+    # Get user with highest activity count
+    most_active_user_id = max(activity_counts, key=activity_counts.get)
+
+    # Get username of most active user
+    cursor.execute('SELECT username FROM people WHERE _id = ?', (most_active_user_id,))
+    most_active_user = cursor.fetchone()[0]
+
+    # Close database connection and return most active user
+    conn.close()
+    return most_active_user
